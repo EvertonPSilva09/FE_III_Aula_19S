e136d992d8c5169ec4aac0585d0109a823a9c8d1
Everton Silva <everton.p.silva@outlook.com>
2023-03-29 22:26:32 -0300

    Adicionando o mycontext no box

--- src/components/box/index.jsx
+++ src/components/box/index.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { MyContext } from "../../context/my-context";
 export default function Box() {
+  const { nome, disciplina } = useContext(MyContext);
   return (
     <div style={{ background: "#999" }}>
-      <h2>Hello Box</h2>
+      <h1>BOX</h1>
+      <h2>Nome: {nome}</h2>
+      <h2>Disciplina: {disciplina}</h2>
     </div>
   );
 }
