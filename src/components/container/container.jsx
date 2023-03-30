7e5b1313cf3db097228775a40c0b33483bb0f609
Everton Silva <everton.p.silva@outlook.com>
2023-03-29 22:26:03 -0300

    Adicionando mycontext no container

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { MyContext } from "../../context/my-context";
+
 export default function Container() {
+  const {nome, disciplina } = useContext(MyContext)
   return (
     <div style={{ background: "#773412" }}>
-      <h2>Hello Container</h2>
+      <h1>CONTAINER</h1>
+      <h2>Nome: {nome}</h2>
+      <h2>Disciplina: {disciplina}</h2>
     </div>
   );
 }
