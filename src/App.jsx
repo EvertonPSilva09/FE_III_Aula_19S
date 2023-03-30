472fb153c0049d1d29f9cbb14699b3bb5da376df
Everton Silva <everton.p.silva@outlook.com>
2023-03-29 22:27:11 -0300

    Adicionando myprovider ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import Box from "./components/box";
 import Container from "./components/container/container";
+import MyProvider from "./context/my-context";
 
 function App() {
   const [value, setValue] = useState(100);
   return (
-    <main>
-      <h1>Hello Mundo</h1>
-      <Box />
-      <Container />
-    </main>
+    <MyProvider>
+      <main>
+        <h1>Hello Mundo</h1>
+        <Box />
+        <Container />
+      </main>
+    </MyProvider>
   );
 }
 
