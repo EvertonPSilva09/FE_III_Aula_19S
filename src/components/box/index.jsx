7603893fe4247c77fbc7332856a9a2e0362d0557
Everton Silva <everton.p.silva@outlook.com>
2023-03-29 21:12:19 -0300

    Arquivos base da aula

---
+++ src/components/box/index.jsx
@@ -0,0 +1,7 @@
+export default function Box() {
+  return (
+    <div style={{ backgtound: "#999" }}>
+      <h2>Hello Box</h2>
+    </div>
+  );
+}
---
+++ src/components/container/container.jsx
@@ -0,0 +1,7 @@
+export default function Container() {
+  return (
+    <div style={{ backgtound: "#773412" }}>
+      <h2>Hello Container</h2>
+    </div>
+  );
+}
